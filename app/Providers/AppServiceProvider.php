e757bd572ddf9e1f3fda3a494068842bcb8d9a93
junko-nakajima <k132528@kansai-u.ac.jp>
2025-01-27 05:15:33 +0000

    blog lists

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Pagination\Paginator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Paginator::useBootstrap();
     }
 
     /**
