30fff73e5ec473cfe4c22d73a12720148981dfea
junko-nakajima <k132528@kansai-u.ac.jp>
2025-03-07 03:29:35 +0000

    API

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,6 +26,15 @@
             @endforeach
             {{ Auth::user()->name }}
         </div>
+        <div>
+            @foreach($questions as $question)
+               <div>
+                   <a href="https://teratail.com/questions/{{ $question['id'] }}>
+                       {{ $question['title'] }}
+                   </a>
+               </div>
+            @endforeach
+        </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
